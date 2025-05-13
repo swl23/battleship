0da86e509138cc3b7104513d73fcd9833ce1bcc3
Steven Long <139284802+swl23@users.noreply.github.com>
2025-05-12 23:10:48 -0700

    test: create tests and class methods for attack operations and ship placement operations

--- game.js
+++ game.js
@@ -2,12 +2,12 @@ class Ship {
     constructor(length) {
         this.length = length;
         this.hits = 0;
-        this.sunk = this.isSunk();
         this.location = null;
+        this.direction = null;
     }
 
     hit() {
-        this.hit += 1;
+        this.hits += 1;
     }
 
     isSunk() {
@@ -27,46 +27,175 @@ class Gameboard {
             this.destroyer = new Ship(3);
             this.submarine = new Ship(3);
             this.patrol = new Ship(2);
+            this.attacksReceived = [];
         }
     }
 
-    place(shipName, coordinates, direction) {
+    place(shipName, headCell, direction) {
         const ship = this[shipName];
-        const valid = confirmValidPlacement(
+        // check that destination coordinates are valid
+        const destinationCoordinates = this.checkIfCoordinatesAreValid(
             ship.length,
-            coordinates,
+            headCell,
             direction
         );
-        if (valid) {
-            ship.location = valid;
+        if (!destinationCoordinates) {
+            throw new Error("Error: coordinates are not valid.");
         } else {
-            throw new Error("Not a valid ship placement!");
+            const shipLocations = this.getShipCells();
+            // if there are no ships placed yet, place this ship
+            if (shipLocations.length === 0) {
+                ship.location = destinationCoordinates;
+            } else {
+                // check for collisions with existing ships
+                const collisions = this.checkForCollisions(
+                    destinationCoordinates,
+                    shipLocations
+                );
+                if (collisions) {
+                    throw new Error("Error: there's already a ship there.");
+                } else {
+                    // check to ensure 1-cell border around placed ships
+                    const touchesAnotherShip = this.checkShipProximities(
+                        destinationCoordinates,
+                        shipLocations
+                    );
+                    if (touchesAnotherShip) {
+                        throw new Error(
+                            "Error: ships must be separated by at least 1 cell."
+                        );
+                    } else {
+                        ship.location = destinationCoordinates;
+                    }
+                }
+            }
         }
     }
-}
 
-function confirmValidPlacement(length, coordinates, direction) {
-    const x = coordinates[0];
-    const y = coordinates[1];
-    const location = [];
-    if (direction === "vertical") {
-        if (0 <= x <= 9 && y <= 9 && y - length + 1 >= 0) {
-            for (let i = 0; i < length; i++) {
-                location.push([x, y - i]);
+    getShips() {
+        return [
+            this.carrier,
+            this.battleship,
+            this.destroyer,
+            this.submarine,
+            this.patrol,
+        ];
+    }
+
+    getShipCells() {
+        const placedShips = this.getShips().filter(
+            (ship) => ship.getLocation() !== null
+        );
+        const cellsOfPlacedShips = [];
+        placedShips.forEach((ship) => {
+            ship.location.forEach((cell) => cellsOfPlacedShips.push(cell));
+        });
+        return cellsOfPlacedShips;
+    }
+
+    checkIfCoordinatesAreValid(length, headCell, direction) {
+        const x = headCell[0];
+        const y = headCell[1];
+        const location = [];
+        if (direction === "vertical") {
+            if (0 <= x <= 9 && y <= 9 && y - length + 1 >= 0) {
+                for (let i = 0; i < length; i++) {
+                    location.push([x, y - i]);
+                }
+            } else {
+                return false;
             }
-        } else {
-            return false;
+        } else if (direction === "horizontal") {
+            if (0 <= y <= 9 && x >= 0 && x + length - 1 <= 9) {
+                for (let i = 0; i < length; i++) {
+                    location.push([x + i, y]);
+                }
+            } else {
+                return false;
+            }
         }
-    } else if (direction === "horizontal") {
-        if (0 <= y <= 9 && x >= 0 && x + length - 1 <= 9) {
-            for (let i = 0; i < length; i++) {
-                location.push([x + i, y]);
+        return location;
+    }
+
+    checkForCollisions(destinationCoordinates, shipLocations) {
+        for (let i = 0; i < destinationCoordinates.length; i++) {
+            if (
+                checkIfCoordinatesAreInArray(
+                    [
+                        destinationCoordinates[i][0],
+                        destinationCoordinates[i][1],
+                    ],
+                    shipLocations
+                )
+            ) {
+                return true;
             }
+        }
+        return false;
+    }
+
+    checkShipProximities(destinationCoordinates, shipLocations) {
+        // make sure there's a 1-cell border around destination coords
+        const cellsAroundDestination = [];
+        destinationCoordinates.forEach((cell) => {
+            let x = cell[0];
+            let y = cell[1];
+            cellsAroundDestination.push(
+                [x + 1, y],
+                [x + 1, y + 1],
+                [x, y + 1],
+                [x - 1, y + 1],
+                [x - 1, y],
+                [x - 1, y - 1],
+                [x, y - 1],
+                [x + 1, y - 1]
+            );
+        });
+        for (let i = 0; i < cellsAroundDestination.length; i++) {
+            if (
+                checkIfCoordinatesAreInArray(
+                    [
+                        cellsAroundDestination[i][0],
+                        cellsAroundDestination[i][1],
+                    ],
+                    shipLocations
+                )
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    receiveAttack(coordinates) {
+        if (
+            this.attacksReceived.length === 0 ||
+            !checkIfCoordinatesAreInArray(coordinates, this.attacksReceived)
+        ) {
+            const ships = this.getShips();
+            for (let i = 0; i < ships.length; i++) {
+                if (
+                    checkIfCoordinatesAreInArray(coordinates, ships[i].location)
+                ) {
+                    ships[i].hit();
+                }
+            }
+            this.attacksReceived.push(coordinates);
+            return;
         } else {
-            return false;
+            throw new Error("Error: that cell has already been attacked.");
         }
     }
-    return location;
+
+    allShipsSunk() {
+        const ships = this.getShips();
+        for (let i = 0; i < ships.length; i++) {
+            if (!ships[i].isSunk()) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 class Player {
@@ -81,3 +210,14 @@ module.exports = {
     Gameboard,
     Player,
 };
+
+function checkIfCoordinatesAreInArray(coordinates, arrayOfCoordinates) {
+    const x = coordinates[0];
+    const y = coordinates[1];
+    for (let i = 0; i < arrayOfCoordinates.length; i++) {
+        if (arrayOfCoordinates[i][0] === x && arrayOfCoordinates[i][1] === y) {
+            return true;
+        }
+    }
+    return false;
+}
--- games.test.js
+++ games.test.js
@@ -2,25 +2,96 @@ const module = require("./game.js");
 
 const Gameboard = module.Gameboard;
 
-test("a ship updates its location property after being placed on board", () => {
+describe("Ship Placement Operations", () => {
+    test("a ship updates its location property when placed in open/valid grid coordinates", () => {
+        const board = new Gameboard();
+        board.place("carrier", [0, 7], "horizontal");
+        expect(board.carrier.getLocation()).toEqual([
+            [0, 7],
+            [1, 7],
+            [2, 7],
+            [3, 7],
+            [4, 7],
+        ]);
+    });
+
+    test("a ship cannot be placed in coordinates that run off the board", () => {
+        const board = new Gameboard();
+        expect(() => board.place("battleship", [9, 5], "horizontal")).toThrow(
+            "Error: coordinates are not valid."
+        );
+    });
+
+    test("a ship cannot be placed on a cell where another ship already exists", () => {
+        const board = new Gameboard();
+        board.place("destroyer", [0, 0], "horizontal");
+        expect(() => board.place("patrol", [2, 0], "horizontal")).toThrow(
+            "Error: there's already a ship there."
+        );
+    });
+
+    test("a ship cannot be placed directly next to another ship", () => {
+        const board = new Gameboard();
+        board.place("destroyer", [0, 0], "horizontal");
+        expect(() => board.place("patrol", [3, 1], "vertical")).toThrow(
+            "Error: ships must be separated by at least 1 cell."
+        );
+    });
+});
+
+describe("Attack Handling Operations", () => {
+    test("the same cell cannot be attacked twice", () => {
+        const board = new Gameboard();
+        board.place("carrier", [0, 9], "vertical");
+        board.place("battleship", [4, 5], "horizontal");
+        board.place("destroyer", [4, 8], "horizontal");
+        board.place("submarine", [1, 3], "horizontal");
+        board.place("patrol", [9, 9], "vertical");
+        board.receiveAttack([3, 2]);
+        expect(() => board.receiveAttack([3, 2])).toThrow(
+            "Error: that cell has already been attacked."
+        );
+    });
+
+    test("a ship's hit counter increments when it is on a cell that receives an attack", () => {
+        const board = new Gameboard();
+        board.place("carrier", [0, 9], "vertical");
+        board.place("battleship", [4, 5], "horizontal");
+        board.place("destroyer", [4, 8], "horizontal");
+        board.place("submarine", [1, 3], "horizontal");
+        board.place("patrol", [9, 9], "vertical");
+        board.receiveAttack([5, 5]);
+        board.receiveAttack([0, 6]);
+        expect(board.carrier.hits).toBe(1);
+        expect(board.battleship.hits).toBe(1);
+        board.receiveAttack([6, 5]);
+        expect(board.battleship.hits).toBe(2);
+    });
+
+    test("a ship is marked as sunk once all its cells have been hit", () => {
+        const board = new Gameboard();
+        board.place("carrier", [0, 9], "vertical");
+        board.place("battleship", [4, 5], "horizontal");
+        board.place("destroyer", [4, 8], "horizontal");
+        board.place("submarine", [1, 3], "horizontal");
+        board.place("patrol", [9, 9], "vertical");
+        board.receiveAttack([9, 9]);
+        expect(board.patrol.isSunk()).toBeFalsy;
+        board.receiveAttack([9, 8]);
+        expect(board.patrol.isSunk()).toBeTruthy;
+    });
+});
+
+test("the board accurately reports whether or not all ships have sunk", () => {
     const board = new Gameboard();
-    board.place("carrier", [0, 7], "horizontal");
-    expect(board.carrier.getLocation()).toEqual([
-        [0, 7],
-        [1, 7],
-        [2, 7],
-        [3, 7],
-        [4, 7],
-    ]);
-    board.place("patrol", [3, 1], "vertical");
-    expect(board.patrol.getLocation()).toEqual([
-        [3, 1],
-        [3, 0],
-    ]);
-    board.place("submarine", [7, 2], "horizontal");
-    expect(board.submarine.getLocation()).toEqual([
-        [7, 2],
-        [8, 2],
-        [9, 2],
-    ]);
+    board.place("carrier", [0, 9], "vertical");
+    board.place("battleship", [2, 9], "vertical");
+    board.place("destroyer", [4, 9], "vertical");
+    board.place("submarine", [6, 9], "vertical");
+    board.place("patrol", [8, 9], "vertical");
+    expect(board.allShipsSunk()).toBe(false);
+    board.getShipCells().forEach((cell) => {
+        board.receiveAttack(cell);
+    });
+    expect(board.allShipsSunk()).toBe(true);
 });
